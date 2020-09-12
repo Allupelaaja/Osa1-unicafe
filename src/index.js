8399ee01d83525acdf1120fa5fbdca8091004d93
Aleksi Heikkilä <aleksi.heikkila@metropolia.fi>
2020-09-12 19:09:45 +0300

    1.6 tehty

--- src/index.js
+++ src/index.js
@@ -1,18 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const Header = (props) => (
+  <h1>{props.name}</h1>
+)
+
+const Stats = (props) => (
+  <div>
+    <p>{props.name1} {props.value1}</p>
+    <p>{props.name2} {props.value2}</p>
+    <p>{props.name3} {props.value3}</p>
+  </div>
+  
+)
+
+const OmaButton = (props) => (
+  <button onClick={props.onClick}>
+    {props.text}
+  </button>
+)
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const headerFeedback = "give feedback"
+  const headerStats = "statistics"
+  const types = [
+    "good", "neutral", "bad"
+  ]
+
+  return (
+    <div>
+      <Header name={headerFeedback}/>
+      <OmaButton onClick ={() => setGood(good + 1)} text={types[0]}/>
+      <OmaButton onClick ={() => setNeutral(neutral + 1)} text={types[1]}/>
+      <OmaButton onClick ={() => setBad(bad + 1)} text={types[2]}/>
+      <Header name={headerStats}/>
+      <Stats name1={types[0]} name2={types[1]} name3={types[2]} value1={good} value2={neutral} value3={bad}/>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, 
   document.getElementById('root')
-);
-//testi
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+)
